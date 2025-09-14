05f77bac46d532d43a404b3e1e90d5c7203ea0a2
Rafael Silva <rafael.silva.06@outlook.com>
2025-09-14 14:27:21 -0600

    Se implementaron los cambios de la clase 4. Estado Local en React: Uso de useState para Contadores

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,6 +6,8 @@ const Counter = () => {
     return (
         <div>
             <p> El contador esta en: {count}</p>
+            <button onClick={() => setCount (count+1)}>Incrementar</button>
+            <button onClick={() => setCount (count-1)}>Decrementar</button>
         </div>
     )
 }
