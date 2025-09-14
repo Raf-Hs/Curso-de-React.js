fa47bce7c3c1aac13580421b7d6764ee36589e1e
Rafael Silva <rafael.silva.06@outlook.com>
2025-09-14 15:07:52 -0600

    Se implementaron los cambios de la clase 7. Uso de useEffect para Manejar Efectos Secundarios en React

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css'
-import NameForm from "./components/nameForm"
+import CounterWithEffect from "./components/CounterWithEffect"
 function App() {
   return (
     <>
-      <h1>Formulario de nombre</h1>
-      <NameForm/>
+      <h1>Formulario de contador</h1>
+      <CounterWithEffect />
     </>
   )  
 }
